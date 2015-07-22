284c6cde40faabf1f54971bf78c51e13427cd717
Christopher Clarke <cclarke@chrisdev.com>
2015-07-22 10:41:02 -0400

    Project refactoring

---
+++ runtests.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import django
+
+from django.conf import settings
+
+
+if not settings.configured:
+    settings_dict = dict(
+        INSTALLED_APPS=(
+            'foundation_formtags',
+            'foundation_formtags.tests',
+        ),
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+                "USER": "",
+                "PASSWORD": "",
+                "HOST": "",
+                "PORT": "",
+            }
+        },
+        MIDDLEWARE_CLASSES = ()
+    )
+
+    settings.configure(**settings_dict)
+    if django.VERSION >= (1, 7):
+        django.setup()
+
+
+def runtests(*test_args):
+    if not test_args:
+        test_args = ['django_pandas']
+
+    parent = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, parent)
+
+    if django.VERSION < (1, 8):
+        from django.test.simple import DjangoTestSuiteRunner
+        failures = DjangoTestSuiteRunner(
+            verbosity=1, interactive=True, failfast=False).run_tests(['tests'])
+        sys.exit(failures)
+
+    else:
+        from django.test.runner import DiscoverRunner
+        failures = DiscoverRunner(
+            verbosity=1, interactive=True, failfast=False).run_tests(test_args)
+        sys.exit(failures)
+
+
+if __name__ == '__main__':
+    runtests()
